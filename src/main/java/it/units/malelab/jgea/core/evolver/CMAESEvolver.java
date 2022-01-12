750a329ddd2692e0ecc024d9ad7b114749843296
Giorgia Nadizar <giorgia.nadizar@gmail.com>
2022-01-12 15:35:39 +0100

    CMA-ES refactor: fields renamed and constructor updated.

--- src/main/java/it/units/malelab/jgea/core/evolver/CMAESEvolver.java
+++ src/main/java/it/units/malelab/jgea/core/evolver/CMAESEvolver.java
@@ -30,55 +30,59 @@ import java.util.function.Function;
 import java.util.logging.Logger;
 import java.util.random.RandomGenerator;
 
+// source -> https://arxiv.org/pdf/1604.00772.pdf
 public class CMAESEvolver<S, F> extends AbstractIterativeEvolver<List<Double>, S, F> {
 
-  // TODO check which of the fields should be part of a run, not ea params (e.g., size)
   private static final Logger L = Logger.getLogger(CMAESEvolver.class.getName());
+
+  // specified
   /**
    * Population size, sample size, number of offspring, λ.
    */
-  protected final int lambda;
+  private final int lambda;
   /**
    * Parent number, number of (positively) selected search points in the population, number of strictly
    * positive recombination weights, µ.
    */
-  protected final int mu;
-  /**
-   * Problem dimensionality.
-   */
-  protected final int size;
+  private final int mu;
   /**
    * Recombination weights.
    */
-  protected final double[] weights;
+  private final double[] weights;
   /**
    * Learning rate for the cumulation for the step-size control.
    */
-  protected final double cs;
-  /**
-   * Learning rate for cumulation for the rank-one update of the covariance matrix.
-   */
-  protected final double cc;
-  /**
-   * The variance effective selection mass for the mean.
-   */
-  protected final double mueff;
-  /**
-   * Expectation of ||N(0,I)|| == norm(randn(N,1))
-   */
-  protected final double chiN;
-  /**
-   * Learning rate for the rank-mu update of the covariance matrix update.
-   */
-  protected final double cmu;
-  /**
-   * Learning rate for the rank-one update of the covariance matrix update.
-   */
-  protected final double c1;
+  private final double cSigma;
   /**
    * Damping parameter for step-size update.
    */
-  protected final double damps;
+  private final double dSigma;
+  /**
+   * Learning rate for cumulation for the rank-one update of the covariance matrix.
+   */
+  private final double cc;
+  /**
+   * Learning rate for the rank-one update of the covariance matrix update.
+   */
+  private final double c1;
+  /**
+   * Learning rate for the rank-mu update of the covariance matrix update.
+   */
+  private final double cMu;
+  /**
+   * The variance effective selection mass for the mean.
+   */
+  private final double muEff;
+
+  /**
+   * Problem dimensionality.
+   */
+  private final int n;
+  /**
+   * Expectation of ||N(0,I)|| == norm(randn(N,1))
+   */
+  private final double chiN;
+
 
   /**
    * Constructs a new CMA-ES instance using default parameters.
@@ -89,55 +93,56 @@ public class CMAESEvolver<S, F> extends AbstractIterativeEvolver<List<Double>, S
       PartialComparator<? super Individual<List<Double>, S, F>> individualComparator
   ) {
     super(solutionMapper, genotypeFactory, individualComparator);
-    this.size = genotypeFactory.build(1, new Random(0)).get(0).size();
-    // initialize selection and recombination parameters
-    lambda = 4 + (int) Math.floor(3 * Math.log(size));
+    n = genotypeFactory.build(1, new Random(0)).get(0).size();
+    chiN = Math.sqrt(n) * (1d - 1d / (4d * n) + 1d / (21d * Math.pow(n, 2)));
+
+    // see Table 1 for details on parameters
+
+    // selection and recombination
+    lambda = 4 + (int) Math.floor(3 * Math.log(n));
     mu = (int) Math.floor(lambda / 2d);
     weights = new double[mu];
-    double sum = 0d;
-    double sumSq = 0d;
+    double sumOfWeights = 0d;
+    double sumOfSquaredWeights = 0d;
     for (int i = 0; i < mu; i++) {
       weights[i] = Math.log((lambda + 1) / 2d) - Math.log(i + 1);
-      sum += weights[i];
-      sumSq += Math.pow(weights[i], 2);
+      sumOfWeights += weights[i];
+      sumOfSquaredWeights += Math.pow(weights[i], 2);
     }
-    // normalize recombination weights array
     for (int i = 0; i < mu; i++) {
-      weights[i] /= sum;
+      weights[i] /= sumOfWeights;
     }
-    mueff = Math.pow(sum, 2) / sumSq;
+    muEff = Math.pow(sumOfWeights, 2) / sumOfSquaredWeights;
 
-    // initialize step-size control parameters
-    cs = (mueff + 2) / (size + mueff + 5);
-    damps = 1 + 2 * Math.max(0, Math.sqrt((mueff - 1) / (size + 1d)) - 1) + cs;
+    // step size control
+    cSigma = (muEff + 2) / (n + muEff + 5);
+    dSigma = 1 + 2 * Math.max(0, Math.sqrt((muEff - 1) / (n + 1d)) - 1) + cSigma;
 
     // initialize covariance matrix adaptation
-    cc = (4d + mueff / size) / (size + 4d + 2 * mueff / size);
-    c1 = 2 / (Math.pow((size + 1.3), 2) + mueff);
-    cmu = Math.min(1 - c1, 2 * (mueff - 2 + 1 / mueff) / (Math.pow((size + 2), 2) + 2 * mueff / 2d));
+    cc = (4d + muEff / n) / (n + 4d + 2 * muEff / n);
+    c1 = 2 / (Math.pow((n + 1.3), 2) + muEff);
+    cMu = Math.min(1 - c1, 2 * (muEff - 2 + 1 / muEff) / (Math.pow((n + 2), 2) + 2 * muEff / 2d));
 
-    // initialize other variables
-    chiN = Math.sqrt(size) * (1d - 1d / (4d * size) + 1d / (21d * Math.pow(size, 2)));
   }
 
   protected class CMAESState extends State {
     // Step-size
     private double stepSize = 0.5;
     // Mean value of the search distribution
-    private double[] distrMean = new double[size];
+    private double[] distrMean = new double[n];
     // Evolution path for step-size
-    private double[] sEvolutionPath = new double[size];
+    private double[] sEvolutionPath = new double[n];
     // Evolution path for covariance matrix, a sequence of successive (normalized) steps, the strategy
     // takes over a number of generations
-    private double[] CEvolutionPath = new double[size];
+    private double[] CEvolutionPath = new double[n];
     // Orthogonal matrix. Columns of B are eigenvectors of C with unit length and correspond to the diagonal
     // element of D
-    private RealMatrix B = MatrixUtils.createRealIdentityMatrix(size);
+    private RealMatrix B = MatrixUtils.createRealIdentityMatrix(n);
     // Diagonal matrix. The diagonal elements of D are square roots of eigenvalues of C and correspond to the
     // respective columns of B
-    private RealMatrix D = MatrixUtils.createRealIdentityMatrix(size);
+    private RealMatrix D = MatrixUtils.createRealIdentityMatrix(n);
     // Covariance matrix at the current generation
-    private RealMatrix C = MatrixUtils.createRealIdentityMatrix(size);
+    private RealMatrix C = MatrixUtils.createRealIdentityMatrix(n);
     // Last generation when the eigendecomposition was calculated
     private int lastEigenUpdate = 0;
 
@@ -259,7 +264,7 @@ public class CMAESEvolver<S, F> extends AbstractIterativeEvolver<List<Double>, S
     // normalized eigenvectors
     RealMatrix B = eig.getV();
     RealMatrix D = eig.getD();
-    for (int i = 0; i < size; i++) {
+    for (int i = 0; i < n; i++) {
       // numerical problem?
       if (D.getEntry(i, i) < 0) {
         L.warning("An eigenvalue has become negative");
@@ -282,9 +287,7 @@ public class CMAESEvolver<S, F> extends AbstractIterativeEvolver<List<Double>, S
     // objective variables initial point
     List<Double> point = genotypeFactory.build(1, random).get(0);
     ((CMAESState) state).setDistrMean(point.stream().mapToDouble(d -> d).toArray());
-    List<Individual<List<Double>, S, F>> population = samplePopulation(
-        fitnessFunction, random, executor, (CMAESState) state);
-    return population;
+    return samplePopulation(fitnessFunction, random, executor, (CMAESState) state);
   }
 
   @Override
@@ -297,19 +300,17 @@ public class CMAESEvolver<S, F> extends AbstractIterativeEvolver<List<Double>, S
   ) throws ExecutionException, InterruptedException {
     updateDistribution(orderedPopulation, (CMAESState) state);
     // update B and D from C
-    if ((state.getIterations() - ((CMAESState) state).getLastEigenUpdate()) > (1d / (c1 + cmu) / size / 10d)) {
+    if ((state.getIterations() - ((CMAESState) state).getLastEigenUpdate()) > (1d / (c1 + cMu) / n / 10d)) {
       eigenDecomposition((CMAESState) state);
     }
     // escape flat fitness, or better terminate?
     if (orderedPopulation.firsts().size() >= Math.ceil(0.7 * lambda)) {
       double stepSize = ((CMAESState) state).getStepSize();
-      stepSize *= Math.exp(0.2 + cs / damps);
+      stepSize *= Math.exp(0.2 + cSigma / dSigma);
       ((CMAESState) state).setStepSize(stepSize);
       L.warning("Flat fitness, consider reformulating the objective");
     }
-    List<Individual<List<Double>, S, F>> newPopulation = samplePopulation(
-        fitnessFunction, random, executor, (CMAESState) state);
-    return newPopulation;
+    return samplePopulation(fitnessFunction, random, executor, (CMAESState) state);
   }
 
   @Override
@@ -326,15 +327,15 @@ public class CMAESEvolver<S, F> extends AbstractIterativeEvolver<List<Double>, S
     List<List<Double>> genotypes = new ArrayList<>();
     while (genotypes.size() < lambda) {
       // new point
-      List<Double> genotype = new ArrayList<>(Collections.nCopies(size, 0d));
+      List<Double> genotype = new ArrayList<>(Collections.nCopies(n, 0d));
       // z ∼ N (0, I) normally distributed vector
-      double[] arz = new double[size];
-      for (int i = 0; i < size; i++) {
+      double[] arz = new double[n];
+      for (int i = 0; i < n; i++) {
         arz[i] = random.nextGaussian();
       }
       // y ∼ N (0, C) y = (B*(D*z))
       double[] ary = state.getB().preMultiply(state.getD().preMultiply(arz));
-      for (int i = 0; i < size; i++) {
+      for (int i = 0; i < n; i++) {
         // add mutation (sigma*B*(D*z))
         genotype.set(i, state.getDistrMean()[i] + state.getStepSize() * ary[i]);
       }
@@ -356,9 +357,9 @@ public class CMAESEvolver<S, F> extends AbstractIterativeEvolver<List<Double>, S
         .toList();
     double[] distrMean = state.getDistrMean();
     double[] oldDistrMean = Arrays.copyOf(distrMean, distrMean.length);
-    double[] artmp = new double[size];
+    double[] artmp = new double[n];
     // recombination
-    for (int i = 0; i < size; i++) {
+    for (int i = 0; i < n; i++) {
       distrMean[i] = 0;
       for (int j = 0; j < mu; j++) {
         distrMean[i] += weights[j] * bestMuPoints.get(j).genotype().get(i);
@@ -373,33 +374,33 @@ public class CMAESEvolver<S, F> extends AbstractIterativeEvolver<List<Double>, S
     // cumulation: update evolution paths
     double[] Bzmean = state.getB().preMultiply(zmean);
     double[] sEvolutionPath = state.getsEvolutionPath();
-    for (int i = 0; i < size; i++) {
-      sEvolutionPath[i] = (1d - cs) * sEvolutionPath[i] + (Math.sqrt(cs * (2d - cs) * mueff)) * Bzmean[i];
+    for (int i = 0; i < n; i++) {
+      sEvolutionPath[i] = (1d - cSigma) * sEvolutionPath[i] + (Math.sqrt(cSigma * (2d - cSigma) * muEff)) * Bzmean[i];
     }
     state.setsEvolutionPath(sEvolutionPath);
 
     // calculate step-size evolution path norm
     double psNorm = 0.0;
-    for (int i = 0; i < size; i++) {
+    for (int i = 0; i < n; i++) {
       psNorm += sEvolutionPath[i] * sEvolutionPath[i];
     }
     psNorm = Math.sqrt(psNorm);
 
     // Heaviside function
     int hsig = 0;
-    if (psNorm / Math.sqrt(1 - Math.pow((1d - cs), 2 * state.getIterations())) / chiN < (1.4 + 2d / (size + 1))) {
+    if (psNorm / Math.sqrt(1 - Math.pow((1d - cSigma), 2 * state.getIterations())) / chiN < (1.4 + 2d / (n + 1))) {
       hsig = 1;
     }
 
     double[] CEvolutionPath = state.getCEvolutionPath();
-    for (int i = 0; i < size; i++) {
-      CEvolutionPath[i] = (1 - cc) * CEvolutionPath[i] + hsig * Math.sqrt(cc * (2 - cc) * mueff) * artmp[i];
+    for (int i = 0; i < n; i++) {
+      CEvolutionPath[i] = (1 - cc) * CEvolutionPath[i] + hsig * Math.sqrt(cc * (2 - cc) * muEff) * artmp[i];
     }
     state.setCEvolutionPath(CEvolutionPath);
 
     RealMatrix C = state.getC();
     // adapt covariance matrix C
-    for (int i = 0; i < size; i++) {
+    for (int i = 0; i < n; i++) {
       for (int j = 0; j <= i; j++) {
         double rankOneUpdate = CEvolutionPath[i] * CEvolutionPath[j] + (1 - hsig) * cc * (2 - cc) * C.getEntry(i, j);
         double rankMuUpdate = 0d;
@@ -410,7 +411,7 @@ public class CMAESEvolver<S, F> extends AbstractIterativeEvolver<List<Double>, S
               .genotype()
               .get(j) - oldDistrMean[j]) / state.getStepSize());
         }
-        C.setEntry(i, j, (1 - c1 - cmu) * C.getEntry(i, j) + c1 * rankOneUpdate + cmu * rankMuUpdate);
+        C.setEntry(i, j, (1 - c1 - cMu) * C.getEntry(i, j) + c1 * rankOneUpdate + cMu * rankMuUpdate);
         if (i != j) {
           // force symmetric matrix
           C.setEntry(j, i, C.getEntry(i, j));
@@ -421,7 +422,7 @@ public class CMAESEvolver<S, F> extends AbstractIterativeEvolver<List<Double>, S
 
     // adapt step size sigma
     double stepSize = state.getStepSize();
-    stepSize *= Math.exp((cs / damps) * ((psNorm / chiN) - 1));
+    stepSize *= Math.exp((cSigma / dSigma) * ((psNorm / chiN) - 1));
     state.setStepSize(stepSize);
   }
 }
